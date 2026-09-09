15f2eada476abf804af930e8771b6fbf3d53f1f6
hagen-o <hagen@hechler-oberacker.de>
2026-09-09 10:55:23 +0200

    schritt 6 fertig

---
+++ src/ai_task_agent/tools/registry.py
@@ -0,0 +1,15 @@
+from ai_task_agent.tools.base import Tool
+
+
+class ToolRegistry:
+    def __init__(self) -> None:
+        self._tools: dict[str, Tool] = {}
+
+    def register(self, tool: Tool) -> None:
+        self._tools[tool.name] = tool
+
+    def get(self, name: str) -> Tool | None:
+        if name not in self._tools:
+            return None
+        else:
+            return self._tools.get(name)
